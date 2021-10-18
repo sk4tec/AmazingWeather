610fe2cdc8edccebee02b0974c4ac3c1eca3f283
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-10-18 21:27:36 +0100

    add: refactored out Network calls

---
+++ Network/Network.swift
@@ -0,0 +1,55 @@
+//
+//  Network.swift
+//  AmazingWeather
+//
+//  Created by Sunjay Kalsi on 18/10/2021.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+struct Network {
+    private let weatherApiKey = "062487fc4b8c1f4cf2c6e584511b54ef"
+    private let apiBaseURL = "https://api.openweathermap.org/data/2.5/weather?q="
+    private let measurementUnits = "&units=metric"
+    
+    func loadWeatherForCities(cites: [String]) -> [WeatherModel]{
+        var cityWeathers = [WeatherModel]()
+        
+        for city in cites {
+            self.loadWeatherData(city: city, completion: { weatherVal in
+                if let weatherVal = weatherVal {
+                    cityWeathers.append(weatherVal)
+                }
+            })
+        }
+        return cityWeathers
+    }
+    
+    
+    func loadWeatherData(city: String, completion: @escaping (WeatherModel?) -> Void) {
+        let completeUrl = apiBaseURL + city + "&appid=" + weatherApiKey + measurementUnits
+        
+        var weatherModel: WeatherModel? = nil
+        
+        AF.request(completeUrl).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                weatherModel = WeatherModel()
+                weatherModel?.locationName = city
+                weatherModel?.temp = json["main"]["temp"].number?.intValue ?? 0
+                weatherModel?.humidity = json["main"]["humidity"].number?.intValue ?? 0
+                weatherModel?.sunriseUnixUtc = json["sys"]["sunrise"].number?.intValue ?? 0
+                weatherModel?.sunsetUnixUtc = json["sys"]["sunset"].number?.intValue ?? 0
+                
+                completion(weatherModel)
+            case .failure(let error):
+                print(error)
+                completion(nil)
+                // we need to load old data from the DB, if we have any?
+            }
+        }
+    }
+}
