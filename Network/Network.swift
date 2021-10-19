8a7f8a63a697cae766739a49b4f2384dd980b11c
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-10-19 20:31:07 +0100

    add: put in the whole json WeatherData class

--- Network/Network.swift
+++ Network/Network.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
-
+//https://openweathermap.org/current
 struct Network {
     private let weatherApiKey = "062487fc4b8c1f4cf2c6e584511b54ef"
     private let apiBaseURL = "https://api.openweathermap.org/data/2.5/weather?q="
@@ -36,6 +36,11 @@ struct Network {
         AF.request(completeUrl).responseJSON { response in
             switch response.result {
             case .success(let value):
+                //decode the whole object
+                let weatherData = try? JSONDecoder().decode(WeatherData.self, from: response.data!)
+                
+                print(weatherData)
+                
                 let json = JSON(value)
                 weatherModel = WeatherModel()
                 weatherModel?.locationName = city
