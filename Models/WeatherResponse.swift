e3efb60bef4650c63271ae891802382398c4d96f
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-10-20 09:35:29 +0100

    Make Weather Response have Optional properties
    As not all of the values come down the JSON wasn't parsing. So ALL of the properties need to be optional so it passes

--- Models/WeatherResponse.swift
+++ Models/WeatherResponse.swift
@@ -13,34 +13,34 @@ import Foundation
 
 // MARK: - WeatherData
 struct WeatherData: Codable {
-    let coord: Coord
-    let weather: [Weather]
-    let base: String
-    let main: Main
-    let visibility: Int
-    let wind: Wind
-    let clouds: Clouds
-    let dt: Int
-    let sys: Sys
-    let timezone, id: Int
-    let name: String
-    let cod: Int
+    let coord: Coord?
+    let weather: [Weather]?
+    let base: String?
+    let main: Main?
+    let visibility: Int?
+    let wind: Wind?
+    let clouds: Clouds?
+    let dt: Int?
+    let sys: Sys?
+    let timezone, id: Int?
+    let name: String?
+    let cod: Int?
 }
 
 // MARK: - Clouds
 struct Clouds: Codable {
-    let all: Int
+    let all: Int?
 }
 
 // MARK: - Coord
 struct Coord: Codable {
-    let lon, lat: Double
+    let lon, lat: Double?
 }
 
 // MARK: - Main
 struct Main: Codable {
-    let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, humidity: Int
+    let temp, feelsLike, tempMin, tempMax: Double?
+    let pressure, humidity: Int?
 
     enum CodingKeys: String, CodingKey {
         case temp
@@ -53,16 +53,16 @@ struct Main: Codable {
 
 // MARK: - Sys
 struct Sys: Codable {
-    let type, id: Int
-    let message: Double
-    let country: String
-    let sunrise, sunset: Int
+    let type, id: Int?
+    let message: Double?
+    let country: String?
+    let sunrise, sunset: Int?
 }
 
 // MARK: - Weather
 struct Weather: Codable {
-    let id: Int
-    let main, weatherDescription, icon: String
+    let id: Int?
+    let main, weatherDescription, icon: String?
 
     enum CodingKeys: String, CodingKey {
         case id, main
@@ -73,6 +73,6 @@ struct Weather: Codable {
 
 // MARK: - Wind
 struct Wind: Codable {
-    let speed: Double
-    let deg: Int
+    let speed: Double?
+    let deg: Int?
 }
